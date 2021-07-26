114aeb9a73344e851ca32fbc4f2954bd113b068d
Di Guan <dguan@purdue.edu>
2021-07-26 22:19:09 +0800

    first commit

---
+++ src/main/java/com/test/TestForSpring01.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * test for spring
+ *
+ * @author Di Guan on 7/26/2021 10:17 PM
+ */
+
+public class TestForSpring01 {
+    public static void main(String[] args) {
+        System.out.println("Spring test");
+    }
+}
